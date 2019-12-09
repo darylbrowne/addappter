59119ff4879dff466bbe7a890caf22f86f79de2f
Daryl Browne <darylbrowne@post.harvard.edu>
2019-12-09 10:29:39 -0500

    Beginning of an Era

---
+++ addappter.js
@@ -0,0 +1,5 @@
+        function addappter(){
+            document.write('<h1>Coming soon...The Addappter');
+            let jsonMicro = arguments[0];
+            console.log(jsonMicro);
+        }
