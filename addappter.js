86d46dd3325a5d32c378c2919e3cb7f69cdb1f93
Daryl Browne <darylbrowne@post.harvard.edu>
2019-12-09 10:40:50 -0500

    Update addappter.js

--- addappter.js
+++ addappter.js
@@ -1,7 +1,4 @@
         function addappter(jsonMicro){
             document.write('<h1>Coming soon...The Addappter');
-            var jsonMicro_sanitized = arguments[0];
-            console.log('jsonMicro...');
-            console.dir(jsonMicro_sanitized);
             console.dir(jsonMicro);
         }
