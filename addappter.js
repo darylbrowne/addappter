935130cccbc961630a03ec129975db0c50dfe48f
Daryl Browne <darylbrowne@post.harvard.edu>
2019-12-11 14:57:32 -0500

    Update addappter.js

--- addappter.js
+++ addappter.js
@@ -1,6 +1,7 @@
 function addappter(jsonMicro){
 
 	function table(jsonMicro) {
+		var jsonInspector = JSON.stringify(jsonMicro, undefined, 4)
 		var tblRow = ``, arrRowObj = jsonMicro['micro']['settings']['records'];		
 		var objHeader = arrRowObj[0]['columns'];
 		var tblHeader = objHeader.map(function (column) {return '<th>' + column + '</th>';}).join('');
@@ -23,7 +24,7 @@ function addappter(jsonMicro){
 				<tbody>${tblRow}</tbody>
 				<tfoot></tfoot>
 			</table>
-			<textarea style="font-family: "Lucida Console", Monaco, monospace;">${jsonMicro}</textarea>`;
+			<textarea style="font-family: "Lucida Console", Monaco, monospace;">${jsonInspector}</textarea>`;
 
 		var content = document.getElementsByTagName("body").item(0);
 		content.insertAdjacentHTML("beforeend", htmlTable);
