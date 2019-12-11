acc8d1349e7b56067b797c353fc95b795f6e21a0
Daryl Browne <darylbrowne@post.harvard.edu>
2019-12-11 12:00:28 -0500

    Update addappter.js

--- addappter.js
+++ addappter.js
@@ -6,7 +6,7 @@ function addappter(jsonMicro){
 				<caption></caption>
 				<colgroup></colgroup>
 				<thead></thead>
-				<tbody>${tbody}</tbody>
+				<tbody></tbody>
 				<tfoot></tfoot>
 			</table>`;
 		var arrCell = [], arrRowObj = jsonMicro['micro']['settings']['records'];		
@@ -22,27 +22,7 @@ function addappter(jsonMicro){
 		}
 	};
 	
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://darylbrowne.github.io/addappter/addappter.html');
-	xhr.send(null);	
-	xhr.onreadystatechange = function () {
-	  var DONE = 4; // readyState 4 means the request is done.
-	  var OK = 200; // status 200 is a successful return.
-	  if (xhr.readyState === DONE) {
-	    if (xhr.status === OK) {
-	      console.log(xhr.responseText); // 'This is the returned text.'
-		var tagString = xhr.responseText;
-		var range = document.createRange();
-		range.selectNode(document.getElementsByTagName("body").item(0));
-		var documentFragment = range.createContextualFragment(tagString);
-		document.body.appendChild(documentFragment);
-		table(jsonMicro);
-	    } else {
-	      console.log('Error: ' + xhr.status); // An error occurred during the request.
-	    }
-	  }
-	};
-            
+	table(jsonMicro);            
 	document.write('<h1>Coming soon...The Addappter</h1>');
         console.dir(jsonMicro);
 }
